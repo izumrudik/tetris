924dbf94c8b4d0dc00e925837830eaeec38aab7b
Федор Трифонов <fedar.tryfanau@outlook.com>
2021-05-02 17:03:17 +0300

    first commit

---
+++ main-graphics.py
@@ -0,0 +1,172 @@
+#%% 
+from tetris import Tetris
+import pygame
+import random
+from threading import Thread
+from time import time
+import random
+
+VOLUME = 15#%
+ROWS = 15
+HEIGHT = 600  # высота игрового окна
+WIDTH = int((HEIGHT/(ROWS))*10)  # ширина игрового окна
+
+FPS = 60 # частота кадров в секунду
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+LEFT_BTN = 4
+RIGHT_BTN = 3
+BOTTOM_BTN = 2
+ROATE_LEFT_BTN     = -1
+ROATE_RIGHT_BTN     = 1
+SCALE = int(WIDTH/10)
+
+
+sprites = {
+	0:BLACK,
+	1:(0,0,255),
+	2:(0,255,0),
+	3:(255,0,0),
+	4:(0,255,255),
+	5:(255,0,255),
+	6:(255,255,0),
+	7:(100,255,50),
+ 
+	-1:(0,0,100),
+	-2:(0,100,0),
+	-3:(100,0,0),
+	-4:(0,100,100),
+	-5:(100,0,100),
+	-6:(100,100,0),
+	-7:(50,100,25)
+ 
+ 
+}
+
+# создаем игру и окно
+pygame.init()
+pygame.mixer.init()  # для звука
+
+sounds = {
+	1:pygame.mixer.Sound("spin.wav")
+
+}
+for i in sounds.keys():
+	sounds[i].set_volume(VOLUME/100)
+
+
+class Window:
+	def __init__(self,tetris):
+		self.tetris = tetris
+
+		self.screen = pygame.display.set_mode((WIDTH, HEIGHT),pygame.RESIZABLE)
+
+		
+
+		pygame.display.set_caption("Tetris")
+		self.clock = pygame.time.Clock()
+		self.running = True
+		
+
+
+	def draw(self):
+		#print("\033[92m FPS \033[0m")
+		self.screen.fill(WHITE)
+
+		self.clock.tick(FPS) #держим фпс
+
+
+
+
+		#event running
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.running = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q: self.tetris.button = ROATE_LEFT_BTN
+				if event.key == pygame.K_e: self.tetris.button = ROATE_RIGHT_BTN
+				if event.key == pygame.K_a: self.tetris.button = LEFT_BTN
+				if event.key == pygame.K_s: self.tetris.button = BOTTOM_BTN
+				if event.key == pygame.K_d: self.tetris.button = RIGHT_BTN
+
+
+
+			if event.type == pygame.VIDEORESIZE:
+				global HEIGHT
+				global WIDTH
+				global SCALE
+
+				HEIGHT_ = self.screen.get_height()
+				WIDTH_  = self.screen.get_width()
+		
+				HEIGHT = HEIGHT_
+				WIDTH= int((HEIGHT/(ROWS))*10)  # ширина игрового окна
+				SCALE = int(WIDTH/10)
+				HEIGHT = SCALE* ROWS
+
+
+				self.screen = pygame.display.set_mode((WIDTH, HEIGHT),pygame.RESIZABLE)
+			
+
+		
+
+		#музыкы
+
+		sound = []
+		i = True
+		while i:
+			i = self.tetris.sound
+			sound.append(i)
+
+		sound = sound[:-1:]
+
+
+		for i in sound:
+			if i in sounds.keys():
+				sounds[i].play()
+			
+		#отрисовка
+
+		main_array = self.tetris.main_array
+		#print(main_array)
+
+		for x in range(len(main_array)):
+			for y in range(len(main_array[x])):
+				self.drawRect(x,y, main_array[x][y])
+				
+
+		
+		pygame.display.flip()
+
+	def drawRect(self,x,y,type):
+
+		x = int(x*(SCALE))
+		y = int((ROWS-(y+1))*(SCALE/2))
+
+		#if type>0: type = random.randint(0,7)
+		pygame.draw.rect(self.screen,sprites.get(type,BLACK),(
+			x,
+			y,
+
+			x+SCALE,
+			y+SCALE
+		
+			))
+
+
+	def run(self):
+		while self.running:
+			self.draw()
+		pygame.quit()
+
+tetris = Tetris(ROWS)
+app = Window(tetris)
+
+
+app.run()
+
+# %%
---
+++ tetris.py
@@ -0,0 +1,377 @@
+#main array anotation:
+# 0 : nothing           color
+# - something: whited   color
+# 1 :bar                color
+# 2 :L                  color
+# 3 :J                  color
+# 4 :T                  color
+# 5 :Z                  color
+# 6 :inverted Z         color
+# 7 :brick              color
+
+#sound annotation:
+#0 - nothing
+#1 - rotate
+#2 - clear line
+
+
+#button annotation:
+# -1-rotate-left
+# 1-rotate-right
+# 2-bottom
+# 3-right
+# 4-left
+
+from random import choice
+
+
+class Tetris:
+	def __init__(self,height=15,speed_frames=120):
+		self.__main_array = [
+			[ 0 for j in range(height) ] for i in range(10)
+		]
+
+		self.__felled_array = [
+			[ 0 for j in range(height) ] for i in range(10)
+		]
+		self.height = height
+		self.__sound = []
+		self.__pack = [1,2,3,4,5,6,7]
+		self.__next_piece = choice(self.__pack)
+		self.__pack.remove(self.__next_piece)
+		self.__buttons = []
+
+		self.__speed_frames = speed_frames
+		self.__frames_passed = 0
+
+
+		self.__current_brick_type = choice(self.__pack)
+		self.__pack.remove(self.__current_brick_type)
+		
+		self.__make_pos()
+		self.__score = 0
+		self.__delete_lines = []
+		self.__current_rotation = 0
+
+	
+	
+# 1 :bar                color
+# 2 :L                  color
+# 3 :J                  color
+# 4 :T                  color
+# 5 :Z                  color
+# 6 :inverted Z         color
+# 7 :brick              color
+
+
+		
+
+	def __die(self):
+		self.__current_brick_pos = []
+		self.die = True
+		#print("died")
+
+		self = self.__init__(self.height,(self.__speed_frames-1))#restart but harder
+
+
+	def __move_brick_by(self,offset):
+		try:
+			copy = [[ 0,0 ] for i in range(4)]
+
+			for i in range(len(self.__current_brick_pos)):
+				copy[i] = self.__current_brick_pos[i].copy()
+
+
+
+			for i in copy: #move
+				i[0] -=offset
+				assert i[0] >=0 and i[0] <=9, "Hit borders"
+				assert not self.__felled_array[i[0]][i[1]], "Hit something"
+				
+				
+			self.__current_brick_pos = copy
+		
+
+		except AssertionError:#hit ground
+			pass
+ 
+	def __move_brick(self):
+
+
+
+		try:
+			copy = [[ 0,0 ] for i in range(4)]
+
+			for i in range(len(self.__current_brick_pos)):
+				copy[i] = self.__current_brick_pos[i].copy()
+
+
+
+			for i in copy: #move
+				i[1] -=1
+				assert not self.__felled_array[i[0]][i[1]], "Hit something"
+				assert i[0]>=0 and i[1]>=0, "hit ground"
+				
+			self.__current_brick_pos = copy
+		
+
+		except AssertionError:#hit ground
+			for i in self.__current_brick_pos:	
+				self.__felled_array[i[0]][i[1]] = self.__current_brick_type # add brick
+			self.__swap_current_brick()
+
+	def __delete_if_possible(self):
+		if not self.__delete_lines:
+			return 0
+		self.__sound.append(2)
+		for line_to_delete in self.__delete_lines:
+			
+			for line_to_move in range(line_to_delete,self.height-1):
+				for x in range(len(self.__felled_array)):
+					self.__felled_array[x][line_to_move] = self.__felled_array[x][line_to_move+1]
+				for x in range(len(self.__felled_array)):
+					self.__felled_array[x][self.height-1] = 0
+		self.__delete_lines = []
+
+
+
+
+	def __check_for_lines(self):
+		y_shaped_array =[[ 0 for i in range(10)]  for j in range(self.height)]
+
+		for x in range(len(self.__felled_array)):
+			for y in range(len(self.__felled_array[x])):
+				y_shaped_array[y][x] = self.__felled_array[x][y]
+
+
+		delete_lines = []
+		for y in range(len(y_shaped_array)):
+			i = True
+			for x in y_shaped_array[y]:
+				i = i and x
+			if i:
+				delete_lines.append(y)
+
+		for x in range(len(self.__felled_array)):
+			for y in range(len(self.__felled_array[x])):
+				if y in delete_lines:
+					self.__felled_array[x][y] = -abs(self.__felled_array[x][y])
+
+		self.__delete_lines = delete_lines
+		#print(delete_lines)
+
+
+	def __swap_current_brick(self):
+		self.__check_for_lines()
+		self.__current_brick_type = self.__next_piece
+		if len(self.__pack) ==0: self.__pack = [1,2,3,4,5,6,7]
+		self.__next_piece = choice(self.__pack)
+		self.__pack.remove(self.__next_piece)
+	
+		self.__make_pos()
+		try:
+			for i in self.__current_brick_pos: #move
+				assert not self.__felled_array[i[0]][i[1]], "Hit something"
+    
+		except AssertionError:
+			self.__die()
+
+
+
+	def __next(self,buttons=()):
+		if 1 in buttons:
+			self.__sound.append(1)
+			self.__current_rotation = (self.__current_rotation+1) % 4 
+			copy = self.__rotate(self.__current_brick_pos)
+			try:
+				for i in copy:
+					assert i[0]>=0 and i[1]>=0 and i[1]<self.height and i[0]<10, "borders"
+					assert not self.__felled_array[i[0]][i[1]], "Hit something"
+				self.__current_brick_pos = copy.copy()
+			except AssertionError:
+				self.__current_rotation = (self.__current_rotation-1) % 4 
+				pass
+
+		if -1 in buttons:
+			self.__sound.append(1)
+			self.__current_rotation = (self.__current_rotation+1) % 4 
+			copy = self.__rotate(self.__current_brick_pos)
+
+			self.__current_rotation = (self.__current_rotation+1) % 4 
+			copy = self.__rotate(copy)
+
+			self.__current_rotation = (self.__current_rotation+1) % 4 
+			copy = self.__rotate(copy)	
+
+			try:
+				for i in copy:
+					assert i[0]>=0 and i[1]>=0 and i[1]<self.height and i[0]<10, "borders"
+					assert not self.__felled_array[i[0]][i[1]], "Hit something"
+					
+				self.__current_brick_pos = copy.copy()
+			except AssertionError:
+				self.__current_rotation = (self.__current_rotation-3) % 4 
+				pass
+
+
+
+		self.__frames_passed +=1
+		if self.__frames_passed >= self.__speed_frames:
+			self.__delete_if_possible()
+			self.__move_brick()
+			self.__frames_passed = 0
+		if 2 in buttons: 
+			self.__delete_if_possible()
+			self.__move_brick()
+		if 3 in buttons: self.__move_brick_by(-1)
+		if 4 in buttons: self.__move_brick_by(1)
+		
+
+		#adding bricks
+
+		copy = [[ 0 for j in range(self.height) ] for i in range(10)]
+		for i in range(len(self.__felled_array)):
+			copy[i] = self.__felled_array[i].copy()
+		
+		for i in self.__current_brick_pos:
+			copy[i[0]][i[1]] = self.__current_brick_type
+			
+			
+			
+		self.__main_array = copy
+
+
+	def __make_pos(self):
+
+		self.__current_rotation = 0
+		height = self.height-1
+		if self.__current_brick_type == 1:
+			self.__current_brick_pos = [ [3,height] ,  [4,height], [5,height], [6,height]]
+			return 0
+		if self.__current_brick_type == 2:
+			self.__current_brick_pos = [[3,height-1], [4,height-1],[5,height-1],[5,height]] 
+			return 0
+		if self.__current_brick_type == 3:
+			self.__current_brick_pos = [ [3,height],[3,height-1], [4,height-1] , [5,height-1]] 
+			return 
+		if self.__current_brick_type == 4:
+			self.__current_brick_pos = [[3,height-1],[4,height-1],[4,height],[5,height-1]] 
+			return 0
+		if self.__current_brick_type == 5:
+			self.__current_brick_pos = [ [3,height-1], [4,height-1],[4,height],[5,height] ] 
+			return 0
+		if self.__current_brick_type == 6:
+			self.__current_brick_pos = [[3,height],[4,height], [4,height-1], [5,height-1] ] 
+			return 0
+		if self.__current_brick_type == 7:
+			self.__current_brick_pos = [[4,height],[4,height-1], [5,height-1],  [5,height]] 
+			return 0
+
+		return 1
+
+	def __rotate(self,brick_pos):
+		
+		pos = brick_pos
+		copy = [i.copy() for i in brick_pos]
+		
+
+		if self.__current_brick_type == 1:#bar #works
+			if self.__current_rotation==0: copy = [ [pos[0][0]-2,pos[0][1]-1] ,[pos[1][0]-1,pos[1][1]], [pos[2][0],pos[2][1]+1],[pos[3][0]+1,pos[3][1]+2] ]
+			if self.__current_rotation==1: copy = [ [pos[0][0]+1,pos[0][1]+1] ,[pos[1][0],pos[1][1]], [pos[2][0]-1,pos[2][1]-1],[pos[3][0]-2,pos[3][1]-2] ]
+			if self.__current_rotation==2: copy = [ [pos[0][0]-1,pos[0][1]-2] ,[pos[1][0],pos[1][1]-1], [pos[2][0]+1,pos[2][1]],[pos[3][0]+2,pos[3][1]+1] ]
+			if self.__current_rotation==3: copy = [ [pos[0][0]+2,pos[0][1]+2] ,[pos[1][0]+1,pos[1][1]+1], [pos[2][0],pos[2][1]],[pos[3][0]-1,pos[3][1]-1] ]
+			
+		if self.__current_brick_type == 2:#L  #WORKS
+			if self.__current_rotation==0: copy = [ [pos[0][0],pos[0][1]-2] ,[pos[1][0]+1,pos[1][1]-1], [pos[2][0]+2,pos[2][1]],[pos[3][0]+1,pos[3][1]+1] ]
+			if self.__current_rotation==1: copy = [ [pos[0][0]+2,pos[0][1]] ,[pos[1][0]+1,pos[1][1]+1], [pos[2][0],pos[2][1]+2],[pos[3][0]-1,pos[3][1]+1] ]
+			if self.__current_rotation==2: copy = [ [pos[0][0],pos[0][1]+2] ,[pos[1][0]-1,pos[1][1]+1], [pos[2][0]-2,pos[2][1]],[pos[3][0]-1,pos[3][1]-1] ]
+			if self.__current_rotation==3: copy = [ [pos[0][0]-2,pos[0][1]] ,[pos[1][0]-1,pos[1][1]-1], [pos[2][0],pos[2][1]-2],[pos[3][0]+1,pos[3][1]-1] ]
+
+		if self.__current_brick_type == 3:#J #works
+			if self.__current_rotation==0: copy = [ [pos[0][0]-1,pos[0][1]-1] ,[pos[1][0],pos[1][1]-2], [pos[2][0]+1,pos[2][1]-1],[pos[3][0]+2,pos[3][1]] ]
+			if self.__current_rotation==1: copy = [ [pos[0][0]+1,pos[0][1]-1] ,[pos[1][0]+2,pos[1][1]], [pos[2][0]+1,pos[2][1]+1],[pos[3][0],pos[3][1]+2] ]
+			if self.__current_rotation==2: copy = [ [pos[0][0]+1,pos[0][1]+1] ,[pos[1][0],pos[1][1]+2], [pos[2][0]-1,pos[2][1]+1],[pos[3][0]-2,pos[3][1]] ]
+			if self.__current_rotation==3: copy = [ [pos[0][0]-1,pos[0][1]+1] ,[pos[1][0]-2,pos[1][1]], [pos[2][0]-1,pos[2][1]-1],[pos[3][0],pos[3][1]-2] ]
+			
+		if self.__current_brick_type == 4:#T #works
+			if self.__current_rotation==0: copy = [ [pos[0][0],pos[0][1]-2] ,[pos[1][0]+1,pos[1][1]-1], [pos[2][0],pos[2][1]],[pos[3][0]+2,pos[3][1]] ]
+			if self.__current_rotation==1: copy = [ [pos[0][0]+2,pos[0][1]] ,[pos[1][0]+1,pos[1][1]+1], [pos[2][0],pos[2][1]],[pos[3][0],pos[3][1]+2] ]
+			if self.__current_rotation==2: copy = [ [pos[0][0],pos[0][1]+2] ,[pos[1][0]-1,pos[1][1]+1], [pos[2][0],pos[2][1]],[pos[3][0]-2,pos[3][1]] ]
+			if self.__current_rotation==3: copy = [ [pos[0][0]-2,pos[0][1]] ,[pos[1][0]-1,pos[1][1]-1], [pos[2][0],pos[2][1]],[pos[3][0],pos[3][1]-2] ]
+			
+		if self.__current_brick_type == 5:#Z # works
+			if self.__current_rotation==0: copy = [ [pos[0][0],pos[0][1]-2] ,[pos[1][0]+1,pos[1][1]-1], [pos[2][0],pos[2][1]],[pos[3][0]+1,pos[3][1]+1] ]
+			if self.__current_rotation==1: copy = [ [pos[0][0]+1,pos[0][1]+2] ,[pos[1][0],pos[1][1]+1], [pos[2][0]+1,pos[2][1]],[pos[3][0],pos[3][1]-1] ]
+			if self.__current_rotation==2: copy = [ [pos[0][0]-1,pos[0][1]-1] ,[pos[1][0],pos[1][1]], [pos[2][0]-1,pos[2][1]+1],[pos[3][0],pos[3][1]+2] ]
+			if self.__current_rotation==3: copy = [ [pos[0][0],pos[0][1]+1] ,[pos[1][0]-1,pos[1][1]], [pos[2][0],pos[2][1]-1],[pos[3][0]-1,pos[3][1]-2] ]
+			
+
+
+		if self.__current_brick_type == 6:#Z inverted
+			if self.__current_rotation==0: copy = [ [pos[0][0],pos[0][1]+1] ,[pos[1][0]+1,pos[1][1]], [pos[2][0],pos[2][1]-1],[pos[3][0]+1,pos[3][1]-2] ]
+			if self.__current_rotation==1: copy = [ [pos[0][0]+1,pos[0][1]-1] ,[pos[1][0],pos[1][1]], [pos[2][0]+1,pos[2][1]+1],[pos[3][0],pos[3][1]+2] ]
+			if self.__current_rotation==2: copy = [ [pos[0][0]-1,pos[0][1]+2] ,[pos[1][0],pos[1][1]+1], [pos[2][0]-1,pos[2][1]],[pos[3][0],pos[3][1]-1] ]
+			if self.__current_rotation==3: copy = [ [pos[0][0],pos[0][1]-2] ,[pos[1][0]-1,pos[1][1]-1], [pos[2][0],pos[2][1]],[pos[3][0]-1,pos[3][1]+1] ]
+			
+
+
+
+
+
+		if self.__current_brick_type == 7:#brick # works #SIMETRIC
+			copy = [ [pos[0][0],pos[0][1]] ,[pos[1][0],pos[1][1]], [pos[2][0],pos[2][1]],[pos[3][0],pos[3][1]] ]
+
+
+
+
+
+		return copy
+
+
+
+
+
+
+
+
+
+
+
+
+# acceseble
+
+
+	@property
+	def button(self):
+		return self.__buttons
+	
+	@button.setter
+	def button(self,value):
+		self.__buttons.append(value)
+
+
+
+	@property
+	def sound(self):
+		if len(self.__sound) >=1:
+			return self.__sound.pop(0)	
+		return 0
+
+	@property
+	def score(self):
+		return self.__score
+
+	@property
+	def next_piece(self):
+		return self.__next_piece
+
+
+	@property
+	def main_array(self):
+		self.__next(self.__buttons)
+		self.__buttons = []
+
+		return self.__main_array
+
+
+	
