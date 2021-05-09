ed81cd48c3e366b631e41effff236eee30ba2bdf
fedor <fedor.tryfanau@gmail.com>
2021-05-10 00:47:19 +0300

    bug fixes and checkpoints

--- bot-neiro.py
+++ bot-neiro.py
@@ -1,29 +1,32 @@
-#%%
-import numpy as np, neat,sys,tetris
+# %%
+import pickle
+import numpy as np
+import neat
+import sys
+import tetris
 from tetris import Tetris
-import pygame,random
+import pygame
+import random
 from threading import Thread
 from time import time
 from os.path import join
-from neat import checkpoint
-
 
 
 tetris.GO_DOWN_SCORE = 1
 tetris.SET_BRICK_SCORE = 10
 
 
-STATE = True#True- generate
-#False - load checkpoint
+STATE = True  # True- generate
+# False - load neat
 
 
 class Game:
-	def __init__(self,idx,p,x,y,width,height,screen,display=True):
+	def __init__(self, idx, p, x, y, width, height, screen, display=True):
 		self.moved = False
 		self.die = False
 		self.display = display
-		self.tetris = Tetris(20,2,20)
-		self.bot = Bot(self.tetris,p) 
+		self.tetris = Tetris(20, 2, 20)
+		self.bot = Bot(self.tetris, p)
 		self.x = x
 		self.y = y
 		self.width = width
@@ -37,22 +40,20 @@ class Game:
 		self.X_OFFSET_BRICKS = 2*self.SCALE
 		self.Y_OFFSET_BRICKS = int(height * 0.05)
 
-
-	
 	def draw(self):
 
-		#self.tetris.FPS = 20 #FPS if 0!=FPS else 60 	#little fix
+		# self.tetris.FPS = 20 #FPS if 0!=FPS else 60 	#little fix
 
-		if not self.die:self.bot.run()
+		if not self.die:
+			self.bot.run()
 		self.tetris.run()
 		if self.tetris.die:
 			self.die = True
 
+		if not self.display:
+			return  # optimization
 
-		if not self.display: return #optimization
-
-		#rip sound		
-
+		# rip sound
 
 		main_array = self.tetris.main_array
 		# print(main_array)
@@ -66,10 +67,12 @@ class Game:
 
 		self.lines_breaked_text = font.render(
 			f"IDX:{self.idx} LINES:{self.tetris.lines_breaked}", True, TEXT_COLOR)
-		self.score_text = font.render(f"SCORE:{self.tetris.score}", True, TEXT_COLOR)
+		self.score_text = font.render(
+			f"SCORE:{self.tetris.score}", True, TEXT_COLOR)
 
 		self.screen.blit(self.score_text, (self.x+self.SCALE*2, self.y))
-		self.screen.blit(self.lines_breaked_text, (self.x+self.SCALE*7, self.y))
+		self.screen.blit(self.lines_breaked_text,
+						 (self.x+self.SCALE*7, self.y))
 
 		# превью следуйщего блока
 		for idx in range(len(self.tetris.next_pieces)):
@@ -77,13 +80,14 @@ class Game:
 			spr = pygame.transform.scale(preview[self.tetris.next_pieces[idx]],
 										 (
 				{0: 0, 1: self.SCALE*1, 2: self.SCALE*2, 3: self.SCALE*2, 4: self.SCALE*2, 5: self.SCALE *
-					2, 6: self.SCALE*2, 7: self.SCALE*2}[self.tetris.next_pieces[idx]],
+				 2, 6: self.SCALE*2, 7: self.SCALE*2}[self.tetris.next_pieces[idx]],
 				{0: 0, 1: self.SCALE*4, 2: self.SCALE*3, 3: self.SCALE*3, 4: self.SCALE*3, 5: self.SCALE *
-					3, 6: self.SCALE*3, 7: self.SCALE*2}[self.tetris.next_pieces[idx]]
+					 3, 6: self.SCALE*3, 7: self.SCALE*2}[self.tetris.next_pieces[idx]]
 			))
 			self.screen.blit(spr, (
 				spr.get_rect(topleft=(
-					self.x+self.SCALE * 10+self.X_OFFSET_BRICKS, self.y+self.Y_OFFSET_BRICKS + 4*self.SCALE*idx
+					self.x+self.SCALE * 10+self.X_OFFSET_BRICKS, self.y +
+					self.Y_OFFSET_BRICKS + 4*self.SCALE*idx
 				))
 			))
 
@@ -91,17 +95,16 @@ class Game:
 		spr = pygame.transform.scale(preview[self.tetris.holded_piece],
 									 (
 			{0: 0, 1: self.SCALE*1, 2: self.SCALE*2, 3: self.SCALE*2, 4: self.SCALE*2, 5: self.SCALE *
-				2, 6: self.SCALE*2, 7: self.SCALE*2}[self.tetris.holded_piece],
+			 2, 6: self.SCALE*2, 7: self.SCALE*2}[self.tetris.holded_piece],
 			{0: 0, 1: self.SCALE*4, 2: self.SCALE*3, 3: self.SCALE*3, 4: self.SCALE*3,
-					5: self.SCALE*3, 6: self.SCALE*3, 7: self.SCALE*2}[self.tetris.holded_piece]
+				 5: self.SCALE*3, 6: self.SCALE*3, 7: self.SCALE*2}[self.tetris.holded_piece]
 		))
 		self.screen.blit(spr, (
 			spr.get_rect(topleft=(
-				self.x+self.SCALE * 5 + self.X_OFFSET_BRICKS, self.y+self.Y_OFFSET_BRICKS+self.SCALE*20
+				self.x+self.SCALE * 5 + self.X_OFFSET_BRICKS, self.y +
+				self.Y_OFFSET_BRICKS+self.SCALE*20
 			))
 		))
-
-
 
 	def drawRect(self, x, y, type):
 
@@ -120,7 +123,7 @@ class Game:
 	def run(self):
 		running = True
 		while running:
-			pygame.display.fill((255,255,255))
+			pygame.display.fill((255, 255, 255))
 			for i in pygame.event.get():
 				if i.type == pygame.QUIT:
 					running = False
@@ -140,93 +143,80 @@ class Game:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Bot:
-	def __init__(self,tetris,net):
+	def __init__(self, tetris, net):
 		self.net = net
 		self.generation = 0
 		self.__tetris = tetris
 
-
-
-
 	def run(self):
 
-
 		inputs = []
-		for i in self.main:inputs.extend(i)
-		inputs.extend([*self.next,self.holded])
+		for i in self.main:
+			inputs.extend(i)
+		inputs.extend([*self.next, self.holded])
 
-
-		
 		for i in range(len(inputs)):
-			#-7 to 7 to -1 to 1
-			inputs[i] = inputs[i]/7 #7 = 1, 0 = 0, -7 = -1
+			# -7 to 7 to -1 to 1
+			inputs[i] = inputs[i]/7  # 7 = 1, 0 = 0, -7 = -1
 			# -7 to 7 to 0 to 1
-			#inputs[i] = (inputs[i]+7) /14# -7 = 0, 0 = 0.5, 7 = 1
+			# inputs[i] = (inputs[i]+7) /14# -7 = 0, 0 = 0.5, 7 = 1
 
-
-		inputs = np.array(inputs,dtype=np.float)#numpy
-
-
+		inputs = np.array(inputs, dtype=np.float)  # numpy
 
 		outputs = self.net.activate(inputs)
 
-
-
-
-		self.net.fitness = self.score
+		
 		# should be 206 inputs
-
 
 		#outputs = 0,0,0,0,0,0
 		#outputs = self.workout(inputs)
 		# should be 6 outputs
 
-
-
-
 		for i in range(len(outputs)):
 			if outputs[i] > 0.5:
 				self.button = -1 if i == 0 else i
 
+	# all bot allowed to look
 
-
-	
-	
-
-
-
-
-
-	#all bot allowed to look
-	@property 
-	def button(self):return
+	@property
+	def button(self): return
 	@button.setter
-	def button(self,value):	self.__tetris.button = value
+	def button(self, value):	self.__tetris.button = value
 	@property
-	def next(self):return self.__tetris.next_pieces#to preview next piece
+	def next(self): return self.__tetris.next_pieces  # to preview next piece
 	@property
-	def holded(self):return self.__tetris.holded_piece#to get holded piece
+	def holded(self): return self.__tetris.holded_piece  # to get holded piece
 	@property
-	def main(self):return self.__tetris.main_array#to get main_array
+	def main(self): return self.__tetris.main_array  # to get main_array
 	@property
-	def score(self):return self.__tetris.score#score
+	def score(self): return self.__tetris.score  # score
 
 
-
-
-
-config_path = join("neiro","config-feedforward.txt")
-config = neat.config.Config(neat.DefaultGenome,neat.DefaultReproduction,neat.DefaultSpeciesSet,neat.DefaultStagnation,config_path)
+config_path = join("neiro", "config-feedforward.txt")
+config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+							neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 p = neat.Population(config)
 
-
-
-
-
+checkpoints = neat.Checkpointer()
+checkpoints.filename_prefix = join("neiro", "checkpoints", "generation - ")
 
 if not STATE:
-	p = checkpoints.restore_checkpoint(join("neiro",input("Enter filename of checkpoint (neiro folder):\n")))
+	p = neat.Checkpointer.restore_checkpoint(
+		join("neiro", "checkpoints",input("Enter filename of checkpoint (neiro folder):\n")))
 
 
 p.add_reporter(neat.StdOutReporter(True))
@@ -234,7 +224,7 @@ stats = neat.StatisticsReporter()
 p.add_reporter(stats)
 
 generation = 0
-#%%
+# %%
 
 pygame.init()
 pygame.mixer.init()  # для звука
@@ -278,98 +268,102 @@ preview = {
 
 width = 350
 height = 2 * width
-screen = pygame.display.set_mode((int(5.2*width),int(height*1.1)),pygame.RESIZABLE)
+screen = pygame.display.set_mode(
+	(int(5.2*width), int(height*1.1)), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 MaxScore = 0
-checkpoints = checkpoint.Checkpointer()
-checkpoints.filename_prefix = join("neiro","checkpoints","generation - ")
 
-#%%
-def run(genomes,config):
+
+# %%
+
+
+def run(genomes, config):
 	global generation
 	global MaxScore
 
 	nets = []
+	
 
 	games = []
 	index = 0
 	for __idx__, g in genomes:
-		
-		net = neat.nn.FeedForwardNetwork.create(g,config)
+
+		net = neat.nn.FeedForwardNetwork.create(g, config)
 		nets.append(net)
 		g.fitness = 0
-		games.append(Game(index,net,index*width,0,width,height,screen,index<5) )
+		games.append(Game(index, net, index*width, 40,
+					 width, height, screen, index < 5))
 		index += 1
-
-
-
-
-
-
 
 	running = True
 
 	font = pygame.font.SysFont(None, 40)
 
-
-
-	
 	MaxGenScore = 0
 	length = len(genomes)
 	screen_width = screen.get_width()
-
+	screen_height = screen.get_height()
 
 	while 1:
 		screen.fill((255, 255, 255))
-		
-		clock.tick(FPS) 
 
+		clock.tick(FPS)
 
 		for i in pygame.event.get():
 			if i.type == pygame.QUIT:
-				checkpoints.save_checkpoint(config,p,genomes,generation)
+				checkpoints.save_checkpoint(config, p, neat.DefaultSpeciesSet(config,stats), p.generation)
+
 			if i.type == pygame.VIDEORESIZE:
 				screen_width = screen.get_width()
+				screen_height = screen.get_height()
 
 		died = 0
 		move_by = 0
-		for idx,i in enumerate(games):
-			if MaxScore < i.tetris.score: MaxScore = i.tetris.score
-			if MaxGenScore < i.tetris.score: MaxGenScore = i.tetris.score
+		for idx, i in enumerate(games):
+			genomes[idx][1].fitness = i.tetris.score
+			if MaxScore < i.tetris.score:
+				MaxScore = i.tetris.score
+			if MaxGenScore < i.tetris.score:
+				MaxGenScore = i.tetris.score
 			i.x -= move_by * width
-			i.display = not(i.x +100 > screen_width or i.moved)
+			i.display = not(i.x + 100 > screen_width or i.moved or i.y + 100 >screen_height)
 			if i.die:
-				died+=1
+				died += 1
 				if not i.moved:
 					i.display = False
 					i.moved = True
-					move_by+=1
+					move_by += 1
 
 			i.draw()
 
-
-
-		if died==length:
-			generation+=1
+		if died == length:
+			generation += 1
 			break
 
+		text = font.render(
+			f"generation:{generation} alive:{length-died} FPS:{int(clock.get_fps())} max:{MaxScore} current max:{MaxGenScore}", True, (10, 0, 255))
 
-
-		text = font.render(f"generation:{generation} alive:{length-died} FPS:{int(clock.get_fps())} max:{MaxScore} current max:{MaxGenScore}",True, (10,0,255))
-
-		screen.blit(text,(width*1.5,height*1.05))
+		screen.blit(text, (0, 0))
 
 		pygame.display.flip()
 
+	# save
+	checkpoints.save_checkpoint(config, p,None, generation)	
 
 
-	#save
-	checkpoints.save_checkpoint(config,p,genomes,generation)
+# %%
+winner = p.run(run, 100)
+win = p.best_genome
+pickle.dump(winner, open('winner.pkl', 'wb'))
+pickle.dump(win, open('real_winner.pkl', 'wb'))
 
-	
-#%%
-best = p.run(run,n=1000000)
-Game(best,100,0,width,height,screen).run()
+
+
+Game(win, 100, 0, width, height, screen).run()
+
+
+
+
 # %%
 pygame.display.quit()
 # %%
